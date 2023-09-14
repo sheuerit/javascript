966275157636b0acb854c846489378d43dccf8f2
sheuerit <138076935+sheuerit@users.noreply.github.com>
2023-09-14 21:46:17 +0900

    Add files via upload

---
+++ Ch03. 자료형/main.js
@@ -0,0 +1,95 @@
+// 데이터 타입
+// - 값의 종류를 의미하고, 기본 타입과 객체 타입으로 나뉜다.
+
+// 기본타입
+// - 값을 하나만 가지며 원시값이라고도 불린다.
+// - number(숫자), string(문자열), boolean, null, undefined, symbol
+
+// 객체타입
+// - 기본 타입을 제외한 값은 모두 객체 타입이다.
+// - Object(객체), Array(배열), Function(함수) 등 다양한 객체가 있다.
+
+// typeof
+// - 피연산자의 데이터 타입을 문자열로 반환한다.
+
+console.log(typeof '안녕');
+
+// 숫자
+// - number 타입이라고 한다.
+// - 자바 혹은 다른 언어는 정수, 데이터 크기에 따라 키워드가 다양하지만 자바스크립트는 그냥 숫자만 적으면 된다.
+//
+// - 숫자 타입에서 특별한 값
+//    Infinity - 양수를 0으로 나눈 경우
+//    -Infinity - 음수를 0으로 나눈 경우
+//    NaN - 문자열을 나눈 경우
+
+let number1 = 1;
+const number2 =  "안녕" / 0;
+console.log(number2);
+
+// 문자열
+// - string 타입이라고 한다.
+// - 문자열을 표현할 땐 작은따옴표(''), 큰따옴표(""), 백틱(``) 사용
+// - 백틱(``)은 ES6버전에서 도입
+
+// 백틱
+// - 여려줄 표현이 가능
+// - 템플릿 문자열을 더 편리하게 사용이 가능
+
+// 템플릿 문자열
+// - 값과 문자열을 + 기호를 사용하여 연결
+// - 백틱을 사용하면 더 편리하게 연결이 가능
+
+let nameA = '홍길동';
+let nameB = "고길동";
+let nameC = `전길동`;
+console.log(typeof nameA);
+
+//백틱은 여러줄 표현이 가능
+let temp1 = `안녕하세요
+반갑습니다`;
+console.log(temp1);
+
+// 템플릿 문자열
+let nameD = "송현석";
+let age = 99;
+console.log("내 이름은" + nameD + "입니다. 나이는" + age + "입니다");
+// 백틱 사용
+console.log(`내 이름은 ${nameD}입니다. 나이는 ${age}입니다`);
+
+
+// 이스케이프 표현
+// - 일반적인 문자 외의 특수 문자는 이스케이프 표현을 사용한다.
+// - \n 줄바꿈
+// - \\ 백스페이스
+// - \" 큰따옴표
+// - \' 작은따옴표
+// - \t 텝
+
+console.log('안녕하세요\t반갑습니다.');
+
+// boolean
+// - true와 false 두가지 값만 가지는 타입
+
+let isTrue = true;
+let isFalse = false;
+console.log(typeof isFalse);
+
+// symbol
+// - 유일한 값을 나타내기 위해 ES6버전에서 도입한 데이터 타입
+// - 고유한 식별자가 필요할 때 사용
+
+let key1 = Symbol("abc");
+let key2 = Symbol("abc");
+console.log(key1 == key2); // symbol 값은 서로 다르다
+
+console.log(key1.description); // symbol 값 출력
+
+// null, undefined
+// - 둘 다 값이 비어있는 상태
+// - null : 값이 없는 상태
+// - undefined : 값을 할당하지 않은 상태
+
+let message1 = null; // 값이 빈 상태
+let message2; // 값이 정의되지 않은 상태
+console.log(typeof message2);
---
+++ Ch04. 연산자/main.js
@@ -0,0 +1,89 @@
+// 연산자
+// - 어떠한 연산을 기호로 사용하여 수행한다.
+
+// 문자열 병합
+// - 문자열은 + 연산자를 사용하여 병합
+
+console.log("안녕" + "하세요");
+console.log(typeof ("1" + 2)); // 문자열과 병합하면 number도 string
+console.log("1" + 2 + 3);  // 123
+console.log( 1 + 2 + "3"); // 33
+
+// 산술 연산자
+// - 덧셈, 뺄셈, 곱셈, 나눗셈
+// x % y : 나머지
+let x = 9;
+let y = 2;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+// 증감 연산자
+// ++x : 전위 증가 (x에 1을 증가시킨 후에 명령문 수행)
+// x++ : 후위 증가 (현재 명령문을 수행 후 x에 1을 증가)
+// --x : 전위 감소 (x에 1을 감소시킨 후에 명령문 수행)
+// x-- : 후위 감소 (현재 명령문을 수행 후 x에 1을 감소)
+
+// let result = ++x + y;
+// let result = x++ + y;
+// let result = --x + y;
+let result = x-- + y;
+
+console.log(result);
+console.log(x);
+console.log(y);
+
+// 비교 연산자 (boolean)
+// a > b , a >= b , a < b , a <= b
+// a == b , a === b(데이터 타입까지 동일)
+// a != b , a !== b(데이터 타입까지 상이)
+
+let a = 2;
+let b = '2';
+console.log(a === b); // false
+console.log(a !== b); // true
+
+// 할당 연산자
+// x += y >> x = x + y
+// x -= y >> x = x - y
+// x *= y >> x = x * y
+// x /= y >> x = x / y
+
+let i = 10;
+let j = 20;
+// i = i + j;
+i += j;
+console.log(i);
+
+// 논리 연산자
+// - boolean 값만 다룰 수 있지만 자바스크립트에서만 모든 타입의 값을 받을 수 있고 결과 역시 모든 타입이 될 수 있다
+// null, 0, NaN, 빈 문자열(""), undefined는 false로 간주
+
+// AND 연산자 (&&) - 피연산자가 모두 true일 경우에 true를 반환하고 나머지는 false로 반환
+// OR 연산자 (||) - 피연산자가 하나라도 true일 경우 true를 반환하고 나머지는 false로 반환
+// Not 연산자 (!) - true일 경우 false를 반환하고, false일 경우 true를 반환한다
+// 단축 평가
+// - AND 연산에서 앞쪽 피연산자가 false일 때, OR 연산에서 앞쪽 피연산자가 true일 때 나머지 피연산자의 평가를 생략하고 그대로 반환한다
+
+let a1 = true && true;
+let a2 = true && false;
+let a3 = true && 3 == 4;
+let a4 = 111 && 222;
+let a5 = 111 && false;
+let a6 = null && undefined;
+console.log(a6);
+
+let o1 = true || false;
+let o2 = false || false;
+let o3 = true || 3 == 4;
+let o4 = 111 || 222;
+let o5 = false || 222;
+let o6 = null || undefined;
+console.log(o6);
+
+let n1 = !true;
+let n2 = !false;
+let n3 = !111;
+console.log(n3);
